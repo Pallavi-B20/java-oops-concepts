686e1e8b6c610196330d57dd793d034fa716fc53
p76893695-hub <p76893695@gmail.com>
2026-04-27 20:17:54 +0530

    Added OOP concept files

---
+++ Polymorphism.java
@@ -0,0 +1,17 @@
+class MathOps {
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+public class Polymorphism {
+    public static void main(String[] args) {
+        MathOps m = new MathOps();
+        System.out.println(m.add(2, 3));
+        System.out.println(m.add(2, 3, 4));
+    }
+}
